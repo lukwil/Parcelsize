7e218d4dbcb3d3bb3508bb3076ef5c4ac1bf6ad8
Lukas Willburger <lukas.willburger@stud.hs-kempten.de>
2019-05-29 22:08:45 +0200

    changed gradle file

--- src/main/Parcelsize.java
+++ src/main/Parcelsize.java
@@ -1,5 +1,6 @@
 package main;//import com.google.gson.Gson;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,89 +10,89 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Properties;
 
+@XmlRootElement
 public class Parcelsize {
-	private String length;
-	private String width;
-	private String height;
-	private String size;
+    public String length;
+    public String width;
+    public String height;
+    public String size;
 
-	public Parcelsize() {
+    public Parcelsize() {
 
-	}
+    }
 
-	public Parcelsize(String length, String width, String height, String size) {
-		this.length = length;
-		this.width = width;
-		this.height = height;
-		this.size = size;
-	}
-
-
-	public void calculateSize() {
-		int girth = (Integer.parseInt(this.getLength())) + (Integer.parseInt(this.getWidth()) * 2) + (Integer.parseInt(this.getHeight()) *2);
-
-		Connection conn = null;
-
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-
-			Properties p = new Properties();
-			try {
-				p.load(new BufferedInputStream(new FileInputStream("java.properties")));
-				String dbPath = p.getProperty("mySqlPath");
-
-				conn = DriverManager.getConnection(dbPath);
-				System.out.println("Database Connection established...");
-				PreparedStatement ps = conn.prepareStatement("SELECT size_type FROM parcel_size WHERE ? BETWEEN girth_min_cm AND girth_max_cm;");
-				ps.setInt(1, girth);
-				ResultSet result = ps.executeQuery();
-
-				//Statement statement = conn.createStatement();
-				//ResultSet result = statement.executeQuery("SELECT size_type FROM parcel_size WHERE 35 BETWEEN girth_min_cm AND girth_max_cm;");
-				result.next();
-				this.size = result.getString(1);
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+    public Parcelsize(String length, String width, String height, String size) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.size = size;
+    }
 
 
+    public void calculateSize() {
+        int girth = (Integer.parseInt(this.getLength())) + (Integer.parseInt(this.getWidth()) * 2) + (Integer.parseInt(this.getHeight()) *2);
 
-		} catch (Exception ex){
-			System.err.println("Cannot connect to database server");
-			ex.printStackTrace();
-		}
-	}
+        Connection conn = null;
 
-	public String getLength() {
-		return length;
-	}
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 
-	public void setLength(String length) {
-		this.length = length;
-	}
+            Properties p = new Properties();
+            try {
+                p.load(new BufferedInputStream(new FileInputStream("java.properties")));
+                String dbPath = p.getProperty("mySqlPath");
 
-	public String getWidth() {
-		return width;
-	}
+                conn = DriverManager.getConnection(dbPath);
+                System.out.println("Database Connection established...");
+                PreparedStatement ps = conn.prepareStatement("SELECT size_type FROM parcel_size WHERE ? BETWEEN girth_min_cm AND girth_max_cm;");
+                ps.setInt(1, girth);
+                ResultSet result = ps.executeQuery();
 
-	public void setWidth(String width) {
-		this.width = width;
-	}
+                //Statement statement = conn.createStatement();
+                //ResultSet result = statement.executeQuery("SELECT size_type FROM parcel_size WHERE 35 BETWEEN girth_min_cm AND girth_max_cm;");
+                result.next();
+                this.size = result.getString(1);
 
-	public String getHeight() {
-		return height;
-	}
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-	public void setHeight(String height) {
-		this.height = height;
-	}
 
-	public String getSize() {
-		return size;
-	}
+        } catch (Exception ex){
+            System.err.println("Cannot connect to database server");
+            ex.printStackTrace();
+        }
+    }
 
-	public void setSize(String size) {
-		this.size = size;
-	}
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
