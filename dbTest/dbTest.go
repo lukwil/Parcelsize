5053a0933aea40572c56548ac69d980cde43f75f
Lukas Willburger <lukas.willburger@stud.hs-kempten.de>
2019-06-03 20:14:44 +0200

    added files to git

---
+++ dbTest/dbTest.go
@@ -0,0 +1,19 @@
+package main
+
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
+
+func main() {
+	db, err := sql.Open("mysql", "root:@/sfta")
+
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+}
